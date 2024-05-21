52fef818e3ead1a9f62d88cd6f89ad67c92c75c4
saleel vt <saleelvt57@gmail.com>
2024-05-21 11:18:24 +0530

    basic DOM element operation added and node sampl project created

---
+++ javaScript/sample.js
@@ -0,0 +1,8 @@
+
+// let array =[2,3,4,5,56]
+
+// let large =Math.max(3,3,3,4,5,3,3)
+// let  large=Math.sqrt(9)
+// console.log('my largest number is the ',large);
+const a = Math.sqrt(9)
+console.log('number',a);
